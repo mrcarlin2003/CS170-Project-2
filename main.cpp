911f50f32dc3e379d03c0e3baa6beff71de69f8c
Michael Carlin <mrcarlin2003@gmail.com>
2025-02-24 16:27:29 -0800

    Added beginning prompt and user input

--- main.cpp
+++ main.cpp
@@ -1,4 +1,31 @@
 #include <iostream>
 #include <string>
-#include <queue>
-#include <set>
+
+using namespace std;
+
+// According to the key, I must use Small 119 and Big 60
+
+int main() {
+    string userFile = "";
+    int userChoice = 0;
+
+    cout << "Welcome to Michael Carlin's Feature Selection Algorithm." << endl;
+    cout << "Type in the name of the file to test: ";
+
+    cin >> userFile;
+
+    cout << endl;
+
+    cout << "Type the number of the algorithm you want to run." << endl << endl;
+
+    cout << "1) Forward Selection" << endl;
+    cout << "2) Backward Elimination" << endl;
+
+    cin >> userChoice;
+    cout << endl;
+
+    cout << "This dataset has [blank] features (not including the class attribute), with [blank] instances." << endl;
+
+
+    return 0;
+}
